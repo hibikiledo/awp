2dbd41e8101865e4a47dc79d0c90e876d69f3000
hibikiledo <nutsgmuic@gmail.com>
2017-06-23 20:37:02 +0700

    working redux

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,13 @@ import { combineReducers, compose, createStore } from 'redux'
 import { firebaseStateReducer, reactReduxFirebase } from 'react-redux-firebase'
 
 import Layout from '../components/layout'
+import { initStore } from '../store'
+import withRedux from 'next-redux-wrapper'
 
-export default () => (
+const RootPage = () => (
   <Layout>
     <h1>First page</h1>
   </Layout>
 );
+
+export default withRedux(initStore)(RootPage)
