bfee55936494bb4797b0bcea98a5780297b924cf
hibikiledo <nutsgmuic@gmail.com>
2017-06-23 20:15:35 +0700

    add first page

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "/Users/nut/awp/pages/index.js?entry";
+
+exports.default = function () {
+  return _react2.default.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    }
+  }, "First Page!");
+};
---
+++ pages/index.js
@@ -0,0 +1,3 @@
+export default () => (
+  <h1>First Page!</h1>
+);
