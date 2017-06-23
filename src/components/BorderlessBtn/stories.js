a88c1f19d97d48286b4ade7755994c87bbafcc9a
Chayapat Chadilalai <chayapat@wongnai.com>
2017-06-24 00:46:05 +0700

    add storybook-host decorator

--- src/components/BorderlessBtn/stories.js
+++ src/components/BorderlessBtn/stories.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import {
-    storiesOf
-} from '@storybook/react';
-
+import {storiesOf} from '@storybook/react';
+import {host} from 'storybook-host';
 import BorderlessBtn from './index';
 
 storiesOf('<BorderlessBtn />')
-    .add('default', () => ( 
+    .addDecorator(host({width: '400px'}))
+    .add('default', () => (
         <BorderlessBtn>Create Room</BorderlessBtn>
     ));
--- src/components/PrimaryBtn/stories.js
+++ src/components/PrimaryBtn/stories.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import {
-    storiesOf
-} from '@storybook/react';
-
+import {storiesOf} from '@storybook/react';
+import {host} from 'storybook-host';
 import PrimaryBtn from './index';
 
 storiesOf('<PrimaryBtn />')
-    .add('default', () => ( 
+    .addDecorator(host({width: '400px'}))
+    .add('default', () => (
         <PrimaryBtn>Join</PrimaryBtn>
     ));
