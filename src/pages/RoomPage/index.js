8ad458fa57023e3a30eb4c43737e1135d4c7a35d
Tanapol Nearunchorn <tanapoln@wongnai.com>
2017-06-24 00:01:01 +0700

    subscribe room data

--- src/pages/RoomPage/index.js
+++ src/pages/RoomPage/index.js
@@ -1,19 +1,71 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import PropTypes from 'prop-types';
 
-class RoomPage extends Component {
-  static propTypes = {
+class RoomPageFirebase extends Component {
+  constructor(props, ctx) {
+    super(props, ctx)
+    this.state = null
   }
 
-  static defaultProps = {
+  componentDidMount() {
+    const id = this.props.roomId
+    const ref = global.firebase.database().ref(`room/${id}`)
+    ref.on('value', (s) => {
+      this.setState(s.val())
+    })
+
+    // setInterval(() => {
+    //   ref.update({
+    //     time: new Date().getTime()
+    //   })
+    // }, 1000)
   }
 
   render() {
+    if (!this.state) {
+      return <div>Loading...</div>
+    }
+
+    // console.log(this.state)
+    const props = this.props
+    const ChildComp = this.props.component
+    return <ChildComp {...props} room={this.state} />
+  }
+}
+
+function RoomPageConnect(fn) {
+  return function(comp) {
+    return function(props) {
+      const id = fn(props)
+      return <RoomPageFirebase roomId={id} component={comp} />
+    }
+  }
+}
+
+class RoomPage extends Component {
+  constructor(props, ctx) {
+    super(props, ctx)
+    this.state = null
+  }
+  
+  render() {
+    console.log(this.props)
     return (
-      <div>Room Page</div>
+      <div>
+        <h1>Room page</h1>
+      </div>
     )
   }
 }
 
-export default RoomPage;
+const mapState = (state, {match}) => {
+  return {
+    roomId: match.params.id
+  }
+};
+
+const mapAction = {}
+
+export default connect(mapState)(RoomPageConnect(({roomId}) => roomId)(RoomPage))
