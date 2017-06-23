a489df80e98c496799ac0d1224293a16d43eecc8
Chitipat <chitipat.toy@gmail.com>
2017-06-24 01:56:29 +0700

    generate room id

--- src/pages/CreateRoomPage/index.js
+++ src/pages/CreateRoomPage/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 class CreateRoomPage extends Component {
 
@@ -12,13 +13,33 @@ class CreateRoomPage extends Component {
         <input type="text" ref={(el) => this.roomName = el} />
         <input type="text" ref={(el) => this.nominateTime = el} />
         <button onClick={() => {
-          global.firebase
-            .database()
-            .ref('room')
-            .push({
-              name: this.roomName.value,
-              nominateTime: this.nominateTime.value
-            })
+            var newRoom = global.firebase.database()
+              .ref('room')
+              .push({
+                name: this.roomName.value,
+                nominateTime: this.nominateTime.value
+              });
+
+            var generateRoomID = function() {
+              for (let i = 0; i < 99999; i++) {
+                var tempID = _.padStart(_.random(0,99999),5,'0');
+                var existRoom = global.firebase.database()
+                  .ref('roomList/')
+                  .equalTo(tempID);
+                if (existRoom !== true || existRoom.status === true) {
+                  return tempID;
+                } else {
+                  console.log('Number of rooms exceed the limit');
+                }
+              }
+            }
+
+            global.firebase.database()
+              .ref('roomList/' + generateRoomID())
+              .set({
+                roomkey: newRoom.key,
+                status: false
+              });
         }}
         >
           Create
