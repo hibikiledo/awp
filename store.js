6968b80e4e7e78440e9439c178db875ec77bfc88
hibikiledo <nutsgmuic@gmail.com>
2017-06-23 20:53:08 +0700

    almost working

--- store.js
+++ store.js
@@ -1,14 +1,30 @@
-import { applyMiddleware, createStore } from 'redux'
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
+import { firebaseStateReducer, reactReduxFirebase } from 'react-redux-firebase'
 
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunkMiddleware from 'redux-thunk'
 
+// global.XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
+
 const initialState = {}
 
-export const reducer = (state = initialState, action) => {
-  return state
-}
+const rootReducer = combineReducers({
+  firebase: firebaseStateReducer
+})
+
+const config = {
+  apiKey: "AIzaSyAQ098JTX35JqDLW4Wg4k-uFiSeF5iHE5U",
+  authDomain: "awp-pwa.firebaseapp.com",
+  databaseURL: "https://awp-pwa.firebaseio.com",
+  projectId: "awp-pwa",
+  storageBucket: "awp-pwa.appspot.com",
+  messagingSenderId: "499655629854"
+};
 
 export const initStore = (initialState = initialState) => {
-  return createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunkMiddleware)))
+  const createStoreWithFirebaseAndThunk = compose(
+    reactReduxFirebase(config),
+    composeWithDevTools(applyMiddleware(thunkMiddleware))
+  )(createStore);
+  return createStoreWithFirebaseAndThunk(rootReducer, initialState, )
 }
