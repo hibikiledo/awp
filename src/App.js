ad0d98dc6650461978311031ff6eb8f9bcd2884f
hibikiledo <nutsgmuic@gmail.com>
2017-06-23 22:27:15 +0700

    work

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import logo from './logo.svg';
+import { push } from 'react-router-redux'
 
 class App extends Component {
   render() {
@@ -12,11 +13,15 @@ class App extends Component {
     return (
       <div className="App">
         <input type="text" />
-        <button>JOIN</button>
+        <button onClick={() => {
+          console.log('clicked')
+          this.props.history.push('/create')
+        }}
+        >JOIN</button>
         <Link to='/create'>Create new room</Link>
       </div>
     );
   }
 }
 
-export default connect(({firebase}) => ({firebase}))(App);
+export default App;
