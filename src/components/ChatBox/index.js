392a85547025ee8e57a11624b6bb03fc191889e6
Tanapol Nearunchorn <tanapoln@wongnai.com>
2017-06-25 02:47:50 +0700

    fix scroll

--- src/components/ChatBox/index.js
+++ src/components/ChatBox/index.js
@@ -4,21 +4,55 @@ import PropTypes from 'prop-types';
 import ChatInput from '../ChatInput';
 import ChatDisplay from '../ChatDisplay';
 import { debounce, map } from 'lodash';
+import './style.css'
 
+function scrollChatToEnd() {
+  const ele = global.document.getElementById('chatMessage')
+  if (!ele) {
+    console.error("Cannot find chat messages")
+    return
+  }
+  ele.scrollIntoView(false)
+}
 
-const Chatbox = ({ messages, sendMessage }) => {
-  messages = messages || []
-  return (
-    <div className="container">
-      {map(messages, (r, i) => <ChatDisplay
-        key={i}
-        flag={r.flag}
-        name={r.name}
-        message={r.payload} />
-      )}
-      <ChatInput onSendIconClick={sendMessage} />
-    </div>
-  );
+class Chatbox extends Component {
+  shouldComponentUpdate(props, state) {
+    if (!props.messages) {
+      return false;
+    }
+    if (!this.props.messages) {
+      return true;
+    }
+    return this.props.messages.length !== props.messages.length
+  }
+
+  componentDidMount() {
+    scrollChatToEnd()
+  }
+
+  componentDidUpdate() {
+    scrollChatToEnd()
+  }
+
+  render() {
+    let { messages, sendMessage } = this.props
+    messages = messages || []
+    return (
+      <div className="container">
+        <div className="messagesCtn">
+          <div className="messagesScroll" id="chatMessage">
+            {map(messages, (r, i) => <ChatDisplay
+              key={i}
+              flag={r.flag}
+              name={r.name}
+              message={r.payload} />
+            )}
+          </div>
+        </div>
+        <ChatInput onSendIconClick={sendMessage} />
+      </div>
+    );
+  }
 }
 
 export default Chatbox;
