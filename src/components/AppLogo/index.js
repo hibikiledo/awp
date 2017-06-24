06fb57f21edded36ff7563314aedcced67240015
Chayapat Chadilalai <chayapat@wongnai.com>
2017-06-24 09:58:09 +0700

    add AppLogo component

---
+++ src/components/AppLogo/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './images/logo.jpg';
+import './style.css';
+
+const AppLogo = () => (
+    <div className="app-logo">
+        <img src={logo} className="image" />
+    </div>
+);
+
+export default AppLogo;
